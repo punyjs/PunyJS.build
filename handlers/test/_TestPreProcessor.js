72a184fd48cb21d86726cd2d04f26a36aa0f3c9a
rmcinto <rodger@geekshake.com>
2020-08-14 08:13:30 -0600

    [patch] Updated TestPreProcessor to add the namespace and id to the test entries

--- handlers/test/_TestPreProcessor.js
+++ handlers/test/_TestPreProcessor.js
@@ -5,6 +5,7 @@
 function _TestPreprocessor(
     promise
     , buildHelpers_docExtractor
+    , utils_funcInspector
     , defaults
 ) {
     /**
@@ -76,7 +77,7 @@ function _TestPreprocessor(
                 tests.push(
                     createTestAsset(
                         entry
-                        , asset.data
+                        , asset
                     )
                 );
             }
@@ -87,27 +88,47 @@ function _TestPreprocessor(
     /**
     * @function
     */
-    function createTestAsset(entry, data) {
-        var test = {};
+    function createTestAsset(docEntry, asset) {
+        var data = asset.data
+        , test = {}
+        , funcMeta;
         //loop through the test properties, skipping the meta properties
-        Object.keys(entry.test)
+        Object.keys(docEntry.test)
         .filter(function filterKeys(key) {
             return cnsts.skipProperties.indexOf(key) === -1;
         })
         .forEach(function forEachKey(key) {
-            var prop = entry.test[key];
+            var prop = docEntry.test[key];
             test[key] = prop.name || prop.desc;
         });
         //pull the test code from the data
         test.data = data.substring(
-            entry.documentEnd
-            , entry.offsetEnd
+            docEntry.documentEnd
+            , docEntry.offsetEnd
         );
         test.data = test.data.match(TRIM_PATT)[1];
 
         //make sure we have a test type
-        if (!test.hasOwnProperty(defaults.type)) {
+        if (!test.hasOwnProperty("type")) {
             test.type = defaults.testEntryType;
+        }
+
+        //get the test function name
+        funcMeta = utils_funcInspector(
+            test.data
+        );
+        test.dependencies = funcMeta.params;
+        //use the function name if the test doesn't have one already
+        if (!test.name) {
+            test.name = funcMeta.name;
+        }
+        //if there isn't a namespace use the file namespace
+        if (!test.namespace) {
+            test.namespace = asset.meta.namespace;
+        }
+        //use the namespace + test name as the id if missing
+        if (!test.id) {
+            test.id = `${test.namespace}.${test.name}`;
         }
 
         return test;
